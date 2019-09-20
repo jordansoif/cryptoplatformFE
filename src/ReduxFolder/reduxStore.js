8c320f354dbcd37694a51af11ee609027ab3c5c8
Jordan <48663692+jordansoif@users.noreply.github.com>
2019-09-20 16:13:24 -0400

    redux store update

--- src/ReduxFolder/reduxStore.js
+++ src/ReduxFolder/reduxStore.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { primaryReducer } from "./reduxReducers";
 import thunk from "redux-thunk";
 
 let store = createStore(
   primaryReducer,
-  applyMiddleware(
-    thunk
-    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 
